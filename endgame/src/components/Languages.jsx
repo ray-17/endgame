b7cd77bcf868a9ea188aeb390dbdc2009306850d
ray-17 <simonsharon144@gmail.com>
2026-03-22 09:32:09 +0100

    introduced dynamic statuses for game won, lost and when each language is eliminated

--- endgame/src/components/Languages.jsx
+++ endgame/src/components/Languages.jsx
@@ -1,18 +1,29 @@
+import clsx from "clsx"
 import { languages } from "../languages"
 
 
-export default function Languages(){
 
-    const languageChips = languages.map((language) => {
+export default function Languages(props){
+
+    const languageChips = languages.map((language, index) => {
+        
         const styles={
             backgroundColor: language.backgroundColor,
             color: language.color
         }
+        const isLost = index < props.wrongGuessesCount
+        
+
+        const className = clsx("language-chip", isLost && "lost")
+
+        
+
         return (
-        <span className="language-chip"
+        <span className={className}
          style={styles}
          key={language.name}>
-            {language.name}</span>
+            {language.name}
+            </span>
     )
     })
     
--- endgame/src/components/Status.jsx
+++ endgame/src/components/Status.jsx
@@ -1,8 +1,47 @@
+import clsx from "clsx"
+import { getFarewellText } from "../utils"
+import { languages } from "../languages"
 
 
 
-export default function Status(){
-    return(
-        <section className="status"><p className="win">You win!</p><p className="winmsg">Well done! 🎉</p></section>
+export default function Status(props) {
+    
+
+    const statusClass = clsx("status", {
+        won: props.isWon,
+        lost: props.isLost,
+        farewell: !props.isOver && props.isLastGuessIncorrect,
+    })
+
+    function renderGameStatus(){
+        if(!props.isOver && props.isLastGuessIncorrect){
+            return <p className="farewell">{getFarewellText(languages[props.wrongGuessesCount - 1].name)}</p>
+        }
+
+        if(props.isWon){
+            return (
+                <>
+                        <h2 className="win">You win!</h2>
+                        <p className="msg">Well done! 🎉</p>
+                    </>
+            )
+        }
+        if(props.isLost){
+            return(
+                <>
+                        <h2 className="lose">Game over!</h2>
+                        <p className="msg">You lose! Better start learning Assembly 😭</p>
+                    </>
+            )
+        }
+        else{
+            return null
+        }
+    }
+
+    return (
+        <section className={statusClass}>
+            {renderGameStatus()}
+        </section>
     )
 }
